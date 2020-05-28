e988ce1f3f74a3b9a72f631e6c2c138aa7f6854c
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-05-28 22:49:59 +0900

    github oauth サーバーの認証を実装

--- index.js
+++ index.js
@@ -1,154 +1,60 @@
-const { ApolloServer } = require('apollo-server');
-const { GraphQLScalarType } = require('graphql');
-const typeDefs = `
-  type User {
-    githubLogin: ID!
-    name: String
-    avatar: String
-    postedPhotos: [Photo!]!
-    inPhotos: [Photo!]!
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { MongoClient } = require('mongodb');
+const { readFileSync } = require('fs');
+const expressPlayground = require('graphql-playground-middleware-express')
+  .default;
+const resolvers = require('./resolvers');
+
+require('dotenv').config();
+var typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
+
+async function start() {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
+  let db;
+
+  try {
+    const client = await MongoClient.connect(MONGO_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = client.db();
+  } catch (error) {
+    console.log(`
+
+      Mongo DB Host not found!
+      please add DB_HOST environment variable to .env file
+      exiting...
+
+    `);
+    process.exit(1);
   }
 
-  scalar DateTime
-  type Photo {
-    id: ID!
-    url: String!
-    name: String!
-    description: String
-    category: PhotoCategory!
-    postedBy: User!
-    taggedUsers: [User!]!
-    created: DateTime!
-  }
-
-  enum PhotoCategory {
-    SELFIE
-    PORTRAIT
-    ACTION
-    LANDSCAPE
-    GRAPHIC
-  }
-
-  input PostPhotoInput {
-    name: String!
-    category: PhotoCategory=PORTRAIT
-    desctiption: String
-  }
-
-  type Mutation {
-    postPhoto(input: PostPhotoInput!): Photo!
-  }
-
-  type Query {
-    totalPhotos: Int!
-    allPhotos(after: DateTime): [Photo!]!
-  }
-
-  # Mutationによって新たに投稿されたPhotoを返す
-  #type Mutation {
-  #  postPhoto(name: String! description: String): Photo!
-  #}
-`;
-
-var _id = 0;
-var users = [
-  { githubLogin: 'mHattrfup', name: 'Mike Hattrup' },
-  { githubLogin: 'gPlake', name: 'Glen Plake' },
-  { githubLogin: 'sSchmidt', name: 'Scot Schmidt' },
-];
-
-var photos = [
-  {
-    id: '1',
-    name: 'Droppong the Heart Chute',
-    description: 'The heart chute is one of my favorite chutes',
-    category: 'ACTION',
-    githubUser: 'gPlake',
-    created: '3-28-1977',
-  },
-  {
-    id: '2',
-    name: 'Enjoying the sunshine',
-    category: 'SELFIE',
-    githubUser: 'sSchmidt',
-    created: '1-2-1985',
-  },
-  {
-    id: '3',
-    name: 'Gunbarrel 25',
-    description: '25 laps on gunbarrel today',
-    category: 'LANDSCAPE',
-    githubUser: 'sSchmidt',
-    created: '2018-04-15T19:09:57.308Z',
-  },
-];
-
-var tags = [
-  { photoID: '1', userID: 'gPlake' },
-  { photoID: '2', userID: 'sSchmidt' },
-  { photoID: '3', userID: 'mHattrfup' },
-  { photoID: '4', userID: 'gPlake' },
-];
-
-const resolvers = {
-  Query: {
-    //  写真を格納するための配列の長さを返す
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos,
-  },
-
-  // postPhotoミューテーションと対応するリゾルバ
-  Mutation: {
-    postPhoto(parent, args) {
-      // 新しい写真を作成し、idを生成する
-      var newPhoto = {
-        id: _id++,
-        ...args.input,
-        created: new Date(),
-      };
-      photos.push(newPhoto);
-      return newPhoto;
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+      const githubToken = req.headers.authorization;
+      const currentUser = await db.collection('users').findOne({ githubToken });
+      return { db, currentUser };
     },
-  },
+  });
 
-  Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
-    postedBy: parent => {
-      return users.find(u => u.githubLogin === parent.githubUser);
-    },
-    taggedUsers: parent =>
-      tags
-        .filter(tag => tag.photoID === parent.id)
-        .map(tag => tag.userID)
-        .map(userID => users.find(u => u.githubLogin === userID)),
-  },
-  User: {
-    postedPhotos: parent => {
-      return photos.filter(p => p.githubUser === parent.githubLogin);
-    },
-    inPhotos: parent =>
-      tags
-        .filter(tag => tag.userID === parent.id)
-        .map(tag => tag.photoID)
-        .map(photoID => photos.find(p => p.id === photoID)),
-  },
-  DateTime: new GraphQLScalarType({
-    name: 'DateTime',
-    description: 'A valid date time value',
-    parseValue: value => new Date(value),
-    serialize: value => new Date(value).toISOString(),
-    parseLiteral: ast => ast.value,
-  }),
-};
+  server.applyMiddleware({ app });
 
-// サーバーのインスタンスを作成
-// その際、typeDefs（スキーマ）とリゾルバを引数にとる
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
 
-// Webサーバーを起動
-server
-  .listen()
-  .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
+  app.get('/', (req, res) => {
+    let url = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user`;
+    res.end(`<a href="${url}">Sign In with Github</a>`);
+  });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(
+      `GraphQL Server running at http://localhost:4000${server.graphqlPath}`,
+    ),
+  );
+}
+
+start();
---
+++ resolvers/Query.js
@@ -0,0 +1,27 @@
+module.exports = {
+  me: (parent, args, { currentUser }) => currentUser,
+  //  写真を格納するための配列の長さを返す
+  totalPhotos: (parent, args, { db }) =>
+    db.collection('photos').estimatedDocumentCount(),
+
+  allPhotos: (parent, args, { db }) =>
+    db
+      .collection('photos')
+      .find()
+      .toArray(),
+
+  Photo: (parent, args, { db }) =>
+    db.collection('photos').findOne({ _id: ObjectID(args.id) }),
+
+  totalUsers: (parent, args, { db }) =>
+    db.collection('users').estimatedDocumentCount(),
+
+  allUsers: (parent, args, { db }) =>
+    db
+      .collection('users')
+      .find()
+      .toArray(),
+
+  User: (parent, args, { db }) =>
+    db.collection('users').findOne({ githubLogin: args.login }),
+};
